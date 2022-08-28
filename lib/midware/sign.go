6a89d2acce65c4c22f043d976099a02ae51a12e2
jiedo <jiedohh@gmail.com>
2022-08-28 14:02:13 +0800

    verify timestamp before check sign

--- lib/midware/sign.go
+++ lib/midware/sign.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"sensiblequery/dao/rdb"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,12 +19,35 @@ func SignSha256(input, key string) string {
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
+func VerifyTsWithTs(ts string, expired time.Duration) bool {
+	ats := []rune(ts)
+	if len(ats) < 14 {
+		return false
+	}
+
+	t, e := time.ParseInLocation("20060102150405.000", fmt.Sprint(string(ats[0:14])+"."+string(ats[14:])), time.UTC)
+	if e != nil {
+		return false
+	}
+	now := time.Now().UTC()
+	if t.After(now.Add(expired)) || t.Before(now.Add(-expired)) {
+		return false
+	}
+	return true
+}
+
 func VerifySignatureForHttpGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := context.Background()
-		path := c.Request.URL.Path
-
 		params := c.Request.URL.Query()
+
+		ts := params.Get("ts")
+		if ok := VerifyTsWithTs(ts, time.Minute*5); !ok {
+			c.JSON(http.StatusForbidden, &Response{Code: -1, Msg: "request expired"})
+			c.Abort()
+			return
+		}
+
 		appid := params.Get("appid")
 		secretKey, err := rdb.UserClient.Get(ctx, "appid:"+appid).Result()
 		if err != nil {
@@ -36,6 +60,7 @@ func VerifySignatureForHttpGet() gin.HandlerFunc {
 		params.Del("sign")
 		paramsStrIgnoreSign := params.Encode()
 
+		path := c.Request.URL.Path
 		signCorrect := SignSha256(path+"?"+paramsStrIgnoreSign, secretKey)
 		if signCorrect != sign {
 			c.JSON(http.StatusForbidden, &Response{Code: -1, Msg: "signature not match"})
